4ba90e8d57f72de8a94230488d7f24b56e39e4c3
Ubuntu <ubuntu@ip-172-30-0-85.ec2.internal>
2015-04-24 16:02:53 +0000

    functions kind of

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+ 
+var app = express();
+var port = process.env.PORT || 3000;
+
+var hellobot = require('./hellobot');
+ 
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+// test route
+app.get('/', function (req, res) { res.status(200).send('Hello world! depr') });
+app.post('/hello', hellobot);
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+ 
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
+});
